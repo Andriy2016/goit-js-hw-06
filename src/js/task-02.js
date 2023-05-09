dce014e21ebeb6fb60938fe3603788a56016133c
Andriy2016 <122678717+Andriy2016@users.noreply.github.com>
2023-05-09 17:03:00 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientList = document.querySelector(`ul#ingredients`);
+
+const ingredientLi = ingredients.map(el=>{
+const itemElement = document.createElement(`li`);
+itemElement.classList.add(`item`);
+itemElement.textContent = el;  
+return itemElement;  })
+
+ingredientList.append(...ingredientLi);
