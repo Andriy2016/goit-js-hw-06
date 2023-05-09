56a402df6eef2e3f0b6f7ede0e625d2c5306642d
Andriy2016 <122678717+Andriy2016@users.noreply.github.com>
2023-05-09 17:03:13 +0300

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyEl = document.querySelector('body')
+const changeColorBtnEl = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+
+changeColorBtnEl.addEventListener('click', onColorChange);
+
+function onColorChange() {
+  const randomColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = randomColor;
+  spanEl.textContent = randomColor;
+}
